a93c976d9bdaba4727667db69fa1fb00b92111ec
CAEUser <CAEAddress@gmail.com>
2016-05-26 20:21:36 +0200

    Generated microservice version 1.0

---
+++ db/FAQ_Microservice_create_tables.sql
@@ -0,0 +1,14 @@
+--
+-- Database Schema:  QustionsRubric 
+-- Automatically generated sql script for the service FAQ_Microservice, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table PostCo-_nContents.
+--
+CREATE TABLE QustionsRubric.PostCo-_nContents (
+  Content text,
+  Id int,
+  CONSTRAINT Id_PK PRIMARY KEY (Id)
+);
+
